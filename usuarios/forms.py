43e3e5cc860050ccad8dea0c28d2f374fa35a8a3
Analice Mello <ana.mello2704@gmail.com>
2024-06-06 21:43:42 -0300

    Alterações no código e personalização do Projeto Alura Space

---
+++ usuarios/forms.py
@@ -0,0 +1,81 @@
+from django import forms
+
+class LoginForms(forms.Form):
+    nome_login = forms.CharField(
+        label = 'Nome de Login',
+        required = True,
+        max_length = 100,
+        widget = forms.TextInput(
+            attrs = {
+                'class':'form-control',
+                'placeholder': 'Digite seu nome de usuário',
+            },
+        ),
+    )
+    senha=forms.CharField(
+        label = 'Senha',
+        required = True,
+        max_length = 30,
+        widget = forms.PasswordInput(
+            attrs = {
+                'class':'form-control',
+                'placeholder':'Digite sua senha',
+            },
+        ),
+    )
+
+class CadastroForms(forms.Form):
+    nome_cadastro = forms.CharField(
+        label = 'Nome de Cadastro',
+        required = True,
+        max_length = 100,
+        widget = forms.TextInput(
+            attrs = {
+                'class':'form-control',
+                'placeholder': 'Digite seu nome de usuário',
+            },
+        ),
+    )
+    email = forms.EmailField(
+        label = 'E-mail',
+        required = True,
+        max_length = 100,
+        widget = forms.EmailInput(
+            attrs = {
+                'class':'form-control',
+                'placeholder': 'Ex.: seuemail@xpto.com',
+            },
+        )
+    )
+    senha_criacao=forms.CharField(
+        label = 'Senha',
+        required = True,
+        max_length = 30,
+        widget = forms.PasswordInput(
+            attrs = {
+                'class':'form-control',
+                'placeholder':'Digite sua senha',
+            },
+        ),
+    )
+    senha_confirmacao=forms.CharField(
+        label = 'Confirmação de Senha',
+        required = True,
+        max_length = 30,
+        widget = forms.PasswordInput(
+            attrs = {
+                'class':'form-control',
+                'placeholder':'Digite sua senha novamente',
+            },
+        ),
+    )
+
+    def clean_nome_cadastro(self):
+        nome = self.cleaned_data.get('nome_cadastro')
+
+        if nome:
+            nome = nome.strip()
+            if ' ' in nome:
+                raise forms.ValidationError('Espaços não são permitidos neste campo')
+            else:
+                return nome
